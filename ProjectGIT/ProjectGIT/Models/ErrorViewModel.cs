041091a4438f5c965390a639e0317b27e42a04ba
VictorJernestrand <56867809+VictorJernestrand@users.noreply.github.com>
2020-02-11 11:10:46 +0100

    solution commit

---
+++ ProjectGIT/ProjectGIT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectGIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
